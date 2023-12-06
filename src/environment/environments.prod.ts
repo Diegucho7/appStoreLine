e1ab5037db2a1465d27c59af5413d4f2d1d21552
Diego <dvelarde140@gmail.com>
2023-12-06 15:30:14 -0500

    cambio enviroment

--- src/environment/environments.prod.ts
+++ src/environment/environments.prod.ts
@@ -1,5 +1,5 @@
  export const environments = {
-     baseUrl: 'http://https://diegucho7.github.io/appStoreLine/'
+     baseUrl: 'https://diegucho7.github.io/appStoreLine/'
  }
 
 // export const environment = {
